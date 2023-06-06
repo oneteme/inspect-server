b45b83ef2f5be62320ffd05d7e44c27aa696bc14
u$f <usf.alami@gmail.com>
2023-06-06 09:58:07 +0200

    edit

---
+++ src/main/java/org/usf/trace/api/server/Utils.java
@@ -0,0 +1,31 @@
+package org.usf.trace.api.server;
+
+import static java.util.Objects.isNull;
+
+import java.util.Collection;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Utils {
+	
+    public static <T> T requireSingle(Collection<T> c){
+    	if(isEmpty(c)) {
+    		return null;
+    	}
+    	if(c.size() > 1) {
+    		throw new RuntimeException("too many result"); //custom exception
+    	}
+    	return c.iterator().next();
+    }
+
+    public static boolean isEmpty(Collection<?> c) {
+    	return isNull(c) || c.isEmpty();
+    }
+
+    public static <T> boolean isEmpty(T[] arr){
+    	return isNull(arr) || arr.length == 0;
+    }
+	
+}
