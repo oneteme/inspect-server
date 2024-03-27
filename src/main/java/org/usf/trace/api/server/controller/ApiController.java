db0a7c261f90a44a1c9387cb31b9cfb5b4170e8c
u$f <usf.alami@gmail.com>
2024-03-27 19:08:31 +0100

    edit

--- src/main/java/org/usf/trace/api/server/controller/ApiController.java
+++ src/main/java/org/usf/trace/api/server/controller/ApiController.java
@@ -1,25 +1,40 @@
 package org.usf.trace.api.server.controller;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.CacheControl;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.usf.trace.api.server.FilterCriteria;
-import org.usf.trace.api.server.RequestDao;
-import org.usf.trace.api.server.SessionQueueService;
-import org.usf.traceapi.core.*;
+import static java.util.Objects.isNull;
+import static org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.ResponseEntity.accepted;
+import static org.springframework.http.ResponseEntity.status;
+import static org.usf.trace.api.server.Utils.requireSingle;
+import static org.usf.traceapi.core.Session.nextId;
 
-import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import static java.util.Objects.isNull;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.http.ResponseEntity.accepted;
-import static org.usf.trace.api.server.Utils.requireSingle;
-import static org.usf.traceapi.core.Session.nextId;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.usf.trace.api.server.FilterCriteria;
+import org.usf.trace.api.server.RequestDao;
+import org.usf.trace.api.server.SessionQueueService;
+import org.usf.traceapi.core.ApiRequest;
+import org.usf.traceapi.core.ApiSession;
+import org.usf.traceapi.core.ApplicationInfo;
+import org.usf.traceapi.core.MainSession;
+import org.usf.traceapi.core.Session;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @CrossOrigin
@@ -35,17 +50,18 @@ public class ApiController {
     public ResponseEntity<Void> saveSession(HttpServletRequest hsr,@RequestBody Session[] sessions) {
         for(Session s : sessions) {
             if(isNull(s.getId())) {
-                if(s instanceof MainSession) {
+                if(s instanceof MainSession ms) {
                     s.setId(nextId()); // safe id set for web collectors
-                    updateRemoteAddress(hsr,(MainSession) s);
+                    updateRemoteAddress(hsr, ms);
                 }
                 else if(s instanceof ApiSession) {
                     log.warn("ApiSesstion id is null : {}", s);
                 }
             }
         }
-        queueService.add(sessions);
-        return accepted().build();
+        return queueService.add(sessions) 
+        		? accepted().build()
+        		: status(SERVICE_UNAVAILABLE).build();
     }
     public void  updateRemoteAddress(HttpServletRequest hsr,  @RequestBody MainSession req) {
     	if(isNull(req.getApplication())) { //set IP address for WABAPP trace
