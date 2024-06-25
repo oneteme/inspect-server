10ec6f464ac75bd30082bc678d4d33ac5cdeca07
u$f <usf.alami@gmail.com>
2024-06-25 11:35:34 +0200

    edit

---
+++ src/main/java/org/usf/trace/api/server/RequestMask.java
@@ -0,0 +1,40 @@
+package org.usf.trace.api.server;
+
+import static org.usf.trace.api.server.Utils.isEmpty;
+
+import org.usf.traceapi.core.Session;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 
+ * @author u$f
+ *
+ */
+@RequiredArgsConstructor
+public enum RequestMask {
+	
+	LOCAL(1), JDBC(2), REST(4), FTP(8), SMTP(0x10);
+	
+	private final int value;
+	
+	public static int mask(Session s) {
+		var v = 0;
+		if(!isEmpty(s.getStages())) {
+			v |= LOCAL.value;
+		}
+		if(!isEmpty(s.getQueries())) {
+			v |= JDBC.value;
+		}
+		if(!isEmpty(s.getRequests())) {
+			v |= REST.value;
+		}
+		if(!isEmpty(s.getFtpRequests())) {
+			v |= FTP.value;
+		}
+		if(!isEmpty(s.getMailRequests())) {
+			v |= SMTP.value;
+		}
+		return v;
+	}
+}
--- src/main/java/org/usf/trace/api/server/model/wrapper/RunnableStageWrapper.java
+++ src/main/java/org/usf/trace/api/server/model/wrapper/RunnableStageWrapper.java
@@ -3,22 +3,22 @@ package org.usf.trace.api.server.model.wrapper;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Delegate;
-import org.usf.traceapi.core.RunnableStage;
+import org.usf.traceapi.core.LocalRequest;
 import org.usf.traceapi.core.SessionStage;
 
 @Getter
 @Setter
 public class RunnableStageWrapper {
     @Delegate
-    private final RunnableStage stage;
+    private final LocalRequest stage;
     private final String parentId;
 
     public RunnableStageWrapper(String parentId){
         this.parentId = parentId;
-        this.stage = new RunnableStage();
+        this.stage = new LocalRequest();
     }
 
-    public RunnableStageWrapper(String parentId, RunnableStage stage) {
+    public RunnableStageWrapper(String parentId, LocalRequest stage) {
         this.parentId = parentId;
         this.stage = stage;
     }
