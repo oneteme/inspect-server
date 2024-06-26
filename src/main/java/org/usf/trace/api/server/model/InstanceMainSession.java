4dcf856f844d3482a09abcfade4ee31fe724f1ee
u$f <usf.alami@gmail.com>
2024-06-26 13:25:46 +0200

    edit

--- src/main/java/org/usf/trace/api/server/model/InstanceMainSession.java
+++ src/main/java/org/usf/trace/api/server/model/InstanceMainSession.java
@@ -3,6 +3,8 @@ package org.usf.trace.api.server.model;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Getter;
 import lombok.Setter;
+
+import org.usf.traceapi.core.InstanceEnvironment;
 import org.usf.traceapi.core.MainSession;
 
 
@@ -12,4 +14,9 @@ import org.usf.traceapi.core.MainSession;
 public class InstanceMainSession extends MainSession implements InstanceSession {
     private String instanceId;
     private String appName;
+    
+    @Deprecated
+	public InstanceEnvironment getApplication() {
+		throw new UnsupportedOperationException();
+	}
 }
--- src/main/java/org/usf/trace/api/server/model/InstanceRestSession.java
+++ src/main/java/org/usf/trace/api/server/model/InstanceRestSession.java
@@ -3,7 +3,7 @@ package org.usf.trace.api.server.model;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Getter;
 import lombok.Setter;
-import org.usf.trace.api.server.model.wrapper.InstanceEnvironmentWrapper;
+import org.usf.traceapi.core.InstanceEnvironment;
 import org.usf.traceapi.core.RestSession;
 
 @JsonTypeName("api")
@@ -12,4 +12,9 @@ import org.usf.traceapi.core.RestSession;
 public class InstanceRestSession extends RestSession implements InstanceSession {
     private String instanceId;
     private String appName;
+    
+    @Deprecated
+	public InstanceEnvironment getApplication() {
+		throw new UnsupportedOperationException();
+	}
 }
