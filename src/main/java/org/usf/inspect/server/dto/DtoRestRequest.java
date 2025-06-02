13f5b6585942cc4872d8066bcfb5bfdee0a3e9c2
YoussefDahi <125285822+YoussefDahi@users.noreply.github.com>
2025-06-02 11:25:10 +0200

    add new fields to DtoRestRequest and update request handling for improved session type support (#178)

--- src/main/java/org/usf/inspect/server/dto/DtoRestRequest.java
+++ src/main/java/org/usf/inspect/server/dto/DtoRestRequest.java
@@ -8,4 +8,7 @@ import org.usf.inspect.server.model.RestRequest;
 @Setter
 public class DtoRestRequest extends RestRequest {
     protected String appName;
+    protected String type;
+    protected String sessionType;
+    protected String parent;
 }
