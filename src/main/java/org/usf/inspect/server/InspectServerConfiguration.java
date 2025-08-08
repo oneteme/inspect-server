54ba4e64c6c627aed1a06d27dfb7fd90906c4592
fufu <antonin2.declerck@gmail.com>
2025-08-08 16:06:54 +0200

    configuration server

---
+++ src/main/java/org/usf/inspect/server/InspectServerConfiguration.java
@@ -0,0 +1,15 @@
+package org.usf.inspect.server;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.usf.inspect.core.SchedulingProperties;
+import org.usf.inspect.core.TracingProperties;
+
+@Setter
+@Getter
+@ToString
+public final class InspectServerConfiguration {
+    private SchedulingProperties scheduling = new SchedulingProperties();
+    private TracingProperties tracing = new TracingProperties();
+}
