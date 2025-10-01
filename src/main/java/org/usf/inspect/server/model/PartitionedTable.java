0c3d2ce6dd59c2a149db6dc77cf7012b8a92cf38
fufu <antonin2.declerck@gmail.com>
2025-10-01 17:01:48 +0200

    No partition for ressusage and event trace (#218)
    * no partition for ressusage and event trace
    
    * no partition for ressusage and event trace

--- src/main/java/org/usf/inspect/server/model/PartitionedTable.java
+++ src/main/java/org/usf/inspect/server/model/PartitionedTable.java
@@ -15,16 +15,14 @@ public enum PartitionedTable {
     REQ_SMTP("e_smtp_rqt"),
     REQ_LDAP("e_ldap_rqt"),
     REQ_FTP("e_ftp_rqt"),
-    REQ_LOCAL("e_lcl_rqt"),
-    RESOURCE_USAGE("e_rsc_usg"),
-    INSTANCE_TRACE("e_ins_trc");
+    REQ_LOCAL("e_lcl_rqt");
 
     static final Set<String> tables = Set.of(
             "e_rst_rqt", "e_main_ses", "e_rst_ses",
             "e_smtp_rqt", "e_smtp_stg", "e_ftp_rqt",
             "e_ftp_stg", "e_ldap_rqt", "e_ldap_stg",
             "e_dtb_rqt", "e_dtb_stg", "e_lcl_rqt",
-            "e_rst_rqt_stg", "e_rst_ses_stg", "e_rsc_usg", "e_ins_trc"
+            "e_rst_rqt_stg", "e_rst_ses_stg"
     );
 
     private final String table;
