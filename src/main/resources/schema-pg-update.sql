858941856ab496d2cfb49d82a64f6bd0adccbf3f
fufu <antonin2.declerck@gmail.com>
2026-06-29 12:08:08 +0200

    script update

--- src/main/resources/schema-pg-update.sql
+++ src/main/resources/schema-pg-update.sql
@@ -1,11 +1,2 @@
--- Update script: modification du type de la colonne cd_ord en bigint
--- Date: 2026-06-29
 
-ALTER TABLE e_rst_ses_stg  ALTER COLUMN cd_ord TYPE bigint USING cd_ord::bigint;
-ALTER TABLE e_rst_rqt_stg  ALTER COLUMN cd_ord TYPE bigint USING cd_ord::bigint;
-ALTER TABLE e_smtp_stg     ALTER COLUMN cd_ord TYPE bigint USING cd_ord::bigint;
-ALTER TABLE e_ftp_stg      ALTER COLUMN cd_ord TYPE bigint USING cd_ord::bigint;
-ALTER TABLE e_ldap_stg     ALTER COLUMN cd_ord TYPE bigint USING cd_ord::bigint;
-ALTER TABLE e_dtb_stg      ALTER COLUMN cd_ord TYPE bigint USING cd_ord::bigint;
-ALTER TABLE e_exc_inf      ALTER COLUMN cd_ord TYPE bigint USING cd_ord::bigint;
 
